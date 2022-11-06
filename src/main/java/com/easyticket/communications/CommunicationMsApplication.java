e9008fdc9f150ba9e4d30e732e2cf1d1ec105c9e
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-11-06 07:48:06 -0300

    FEAT adds enviroment variables to connect to email and RabbitMQ

---
+++ src/main/java/com/easyticket/communications/CommunicationMsApplication.java
@@ -0,0 +1,13 @@
+package com.easyticket.communications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyticket/communications/CommunicationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyticket.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
